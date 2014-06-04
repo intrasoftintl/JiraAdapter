e35fdf547b4211bbfaaab3143c4c1f0848b7dc49
eleni <fwtopoulou@gmail.com>
2014-06-04 16:25:45 +0300

    update spring framework version

--- src/test/java/JiraAdapterTest.java
+++ src/test/java/JiraAdapterTest.java
@@ -22,10 +22,21 @@ public class JiraAdapterTest {
     String bindedSystemURL = "http://95.211.223.9:8084";
     String credentials = "soaptester:soaptester";
 
-    // Test uqasar db connection
-    // Test   metrics : { RESOURCES_PER_BINDING , ISSUES_PER_RESOURCE_PER_BINDING ....}
+    /*
+     Test uqasar db connection
+     Test   metrics :
+    {
+      PROJECTS_PER_SYSTEM_INSTANCE ,
+      ISSUES_PER_PROJECTS_PER_SYSTEM_INSTANCE,
+      FIXED_ISSUES_PER_PROJECT,
+      UNRESOLVED_ISSUES_PER_PROJECT,
+      UNRESOLVED_BUG_ISSUES_PER_PROJECT,
+      UNRESOLVED_TASK_ISSUES_PER_PROJECT
+    }
+    */
+
     @Test
-    public void queryTest(){
+    public void queryTestAllMetrics(){
         List<Measurement> measurements = null;
 
 
@@ -42,6 +53,80 @@ public class JiraAdapterTest {
 
 
     }
+
+
+    @Test
+    public void queryTestPROJECTS_PER_SYSTEM_INSTANCE(){
+        List<Measurement> measurements = null;
+            try{
+                measurements = jiraAdapter.query(bindedSystemURL, credentials, "PROJECTS_PER_SYSTEM_INSTANCE");
+                jiraAdapter.printMeasurements(measurements);
+            }catch (uQasarException e){
+                System.out.println(e.toString());
+            }
+    }
+
+
+    @Test
+    public void queryTestISSUES_PER_PROJECTS_PER_SYSTEM_INSTANCE(){
+        List<Measurement> measurements = null;
+        try{
+            measurements = jiraAdapter.query(bindedSystemURL, credentials, "ISSUES_PER_PROJECTS_PER_SYSTEM_INSTANCE");
+            jiraAdapter.printMeasurements(measurements);
+        }catch (uQasarException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void queryTestFIXED_ISSUES_PER_PROJECT(){
+        List<Measurement> measurements = null;
+        try{
+            measurements = jiraAdapter.query(bindedSystemURL, credentials, "FIXED_ISSUES_PER_PROJECT");
+            jiraAdapter.printMeasurements(measurements);
+        }catch (uQasarException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void queryTestUNRESOLVED_ISSUES_PER_PROJECT(){
+        List<Measurement> measurements = null;
+        try{
+            measurements = jiraAdapter.query(bindedSystemURL, credentials, "UNRESOLVED_ISSUES_PER_PROJECT");
+            jiraAdapter.printMeasurements(measurements);
+        }catch (uQasarException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void queryTestUNRESOLVED_BUG_ISSUES_PER_PROJECT(){
+        List<Measurement> measurements = null;
+        try{
+            measurements = jiraAdapter.query(bindedSystemURL, credentials, "UNRESOLVED_BUG_ISSUES_PER_PROJECT");
+            jiraAdapter.printMeasurements(measurements);
+        }catch (uQasarException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void queryTestUNRESOLVED_TASK_ISSUES_PER_PROJECT(){
+        List<Measurement> measurements = null;
+        try{
+            measurements = jiraAdapter.query(bindedSystemURL, credentials, "UNRESOLVED_TASK_ISSUES_PER_PROJECT");
+            jiraAdapter.printMeasurements(measurements);
+        }catch (uQasarException e){
+            System.out.println(e.toString());
+        }
+    }
+
+
+
+
+
+
 
 
     // Try to pass a non existing metric
